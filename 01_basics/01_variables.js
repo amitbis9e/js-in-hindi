b3c70dc8a1dd2dce47abcda1fd10f60c6518cff8
amitbis9e <148409640+amitbis9e@users.noreply.github.com>
2026-05-29 12:48:51 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,5 +3,9 @@ let accountMail = "ncert@gmail.com"
 var accountPassword = "12345"
 accountCity = "bombay"
 
+ let city; // undefined variable
+
+/* prefer not to use var variable instend use let*/
+
 console.log(accountId)
 console.table([accountId, accountMail, accountPassword, accountCity])
